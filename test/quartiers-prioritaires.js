e984099297b18b028c289816e0143bc60d3561c7
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-11-26 19:09:10 +0100

    Add tests for quartiers prioritaires

---
+++ test/quartiers-prioritaires.js
@@ -0,0 +1,107 @@
+/* eslint-env node, mocha */
+const bboxPolygon = require('turf').bboxPolygon;
+const geojsonhint = require('geojsonhint').hint;
+const request = require('supertest');
+const server = require('../server');
+const expect = require('expect.js');
+const _ = require('lodash');
+
+function returnValidGeoJSON(res) {
+    expect(geojsonhint(res.body)).to.eql([]);
+}
+
+describe('/quartiers-prioritaires', () => {
+    describe('/search', () => {
+        function makeRequestWithBbox(bbox) {
+            return request(server)
+                .post('/quartiers-prioritaires/search')
+                .send({ geo: bboxPolygon(bbox).geometry })
+        }
+        describe('not valid input type', () => {
+            it('should reply with 400', done => {
+                request(server)
+                    .post('/quartiers-prioritaires/search')
+                    .send('poiuh')
+                    .expect(400, done);
+            });
+        });
+        describe('request without geo', () => {
+            it('should reply with 400', done => {
+                request(server)
+                    .post('/quartiers-prioritaires/search')
+                    .send({ hello: 'coucou' })
+                    .expect(400, done);
+            });
+        });
+        describe('valid request', () => {
+            it('should reply with 200', done => {
+                request(server)
+                    .post('/quartiers-prioritaires/search')
+                    .send({ geo: bboxPolygon([0, 0, 10, 10]).geometry })
+                    .expect(200, done);
+            });
+            it('should reply with valid GeoJSON', done => {
+                request(server)
+                    .post('/quartiers-prioritaires/search')
+                    .send({ geo: bboxPolygon([0, 0, 10, 10]).geometry })
+                    .expect(returnValidGeoJSON)
+                    .expect(200, done);
+            });
+        });
+        describe('valid geo request which intersect one QP', () => {
+            const makeRequest = () => {
+                return makeRequestWithBbox([
+                    6.200065612792969,
+                    49.09578937341408,
+                    6.262550354003906,
+                    49.12410690961101
+                ])
+                    .expect(200)
+                    .expect(returnValidGeoJSON);
+            };
+            it('should reply with a FeatureCollection containing one Feature', done => {
+                makeRequest()
+                    .expect(res => {
+                        expect(res.body.type).to.eql('FeatureCollection');
+                        expect(res.body.features).to.be.an('array');
+                        expect(res.body.features.length).to.eql(1);
+                    })
+                    .end(done);
+            });
+            it('should reply a FeatureCollection containing a valid Feature', done => {
+                makeRequest()
+                    .expect(res => {
+                        const feature = res.body.features[0];
+                        expect(feature.geometry.type).to.eql('MultiPolygon');
+                        expect(feature.properties).to.eql({
+                          code: 'QP057020',
+                          nom: 'Borny',
+                          commune: 'Metz'
+                        });
+                    })
+                    .end(done);
+            });
+        });
+        describe('valid geo request which does not intersect with any QP', () => {
+            const makeRequest = () => {
+                return makeRequestWithBbox([
+                    6.395416259765625,
+                    49.05744506755445,
+                    6.425628662109375,
+                    49.071391876307196
+                ])
+                    .expect(200)
+                    .expect(returnValidGeoJSON);
+            };
+            it('should reply with an empty FeatureCollection', done => {
+                makeRequest()
+                    .expect(res => {
+                        expect(res.body.type).to.eql('FeatureCollection');
+                        expect(res.body.features).to.be.an('array');
+                        expect(res.body.features.length).to.eql(0);
+                    })
+                    .end(done);
+            });
+        });
+    });
+});
