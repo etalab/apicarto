dc84dbd62dc28b97e7052938991e3b1a06cd750f
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-12-08 21:15:16 +0100

    Update Appellations data

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
         'communes-ign-metrocorse': refDataDir + '/COMMUNE_PARCELLAIRE_METROCORSE.zip',
         'communes-ign-reunion': refDataDir + '/COMMUNE_PARCELLAIRE_REUNION.zip',
         'communes-osm': refDataDir + '/communes-20150101-5m-shp.zip',
-        'appellations-viticoles': refDataDir + '/Appellation_20151207.zip'
+        'appellations-viticoles': refDataDir + '/Appellation_20151208.zip'
     };
 
     const rmdir = {};
@@ -50,7 +50,7 @@ module.exports = function (grunt) {
             select: 'insee,nom'
         },
         'appellations-viticoles': {
-            dataSource: '/vsizip/Appellation_20151207.zip',
+            dataSource: '/vsizip/Appellation_20151208.zip',
             layerName: 'appellation',
             convertToWgs84: true,
             spatialIndex: 'NO',
