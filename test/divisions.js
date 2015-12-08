b0540057bebeb8bbdf509d7f151780e6a20387c6
vinsag <vinsag@yahoo.com>
2015-12-08 16:22:57 +0100

    Modification scenario test et ajout test commune geometrie

--- test/divisions.js
+++ test/divisions.js
@@ -1,41 +1,54 @@
 /*eslint-env node, mocha */
 //var test = require('unit.js');
-var geojson_intersect = require('../lib/geojson_intersection.js') ;
-var CadastreClient = require('../lib/CadastreClient.js');
 
-const geojsonhint = require('geojsonhint').hint;
+//const geojsonhint = require('geojsonhint').hint;
 const request = require('supertest');
 const server = require('../server');
 const expect = require('expect.js');
-const _ = require('lodash');
-
-function returnValidGeoJSON(res) {
-    expect(geojsonhint(res.body)).to.eql([]);
-}
 
 
 describe('test du module divisions', function() {
     
     describe('not valid input type', function() {
         it('should reply with 400 pour numero sur 2 caractères au lieu de 4', function(done){
-		    request(server)
+            request(server)
                 .get('/cadastre/division?insee=94067&numero=02')
                 .expect(400,done);
         });
         
         it('should reply with 400 pour chaine testapi', function(done){
-		    request(server)
+            request(server)
                 .get('/cadastre/division?insee=testapi')
                 .expect(400,done);
         });
-     });
+    });
     describe('Test du module divisions avec valeur', function() {
         it('should work for insee 94067 et section=0A', function(){
             request(server)
                 .get('/cadastre/division?insee=94067&section=0A')
                 .expect(200);
         });
-     });
+        it('should reply a FeatureCollection containing a valid Feature', done => {
+            request(server)    
+                .get('/cadastre/division?insee=94067&section=0A')
+                .expect(res => {
+                    const feature = res.body.features[0];
+                    expect(feature.geometry.type).to.eql('MultiPolygon');
+                    expect(feature.properties).to.eql({
+                        feuille: 1,
+                        section: '0A',
+                        code_dep: '94',
+                        nom_com: 'Saint-Mandé',
+                        code_com: '067',
+                        com_abs: '000',
+                        echelle: '500',
+                        edition: 3,
+                        code_arr: '000' 
+                    });
+                })
+                .end(done);
+        });
+    });
 });
 
 
--- test/parcelle.js
+++ test/parcelle.js
@@ -1,35 +1,27 @@
 /*eslint-env node, mocha */
 //var test = require('unit.js');
-var geojson_intersect = require('../lib/geojson_intersection.js') ;
-var CadastreClient = require('../lib/CadastreClient.js');
 
-const geojsonhint = require('geojsonhint').hint;
 const request = require('supertest');
 const server = require('../server');
-const expect = require('expect.js');
-const _ = require('lodash');
-
-function returnValidGeoJSON(res) {
-    expect(geojsonhint(res.body)).to.eql([]);
-}
+//const expect = require('expect.js');
 
 
 describe('test du module parcelle', function() {
     
     describe('not valid input type', function() {
         it('should reply with 400 pour chaine testapi', function(done){
-		    request(server)
+            request(server)
                 .get('/cadastre/parcelle?insee=testapi')
                 .expect(400,done);
         });
-     });
+    });
     describe('Test du module divisions avec valeur', function() {
-		it('should work for insee 94067 et section=0A', function(){
+        it('should work for insee 94067 et section=0A', function(){
             request(server)
                 .get('/cadastre/parcelle?insee=94067&section=0A')
                 .expect(200);
         });
-     });
+    });
 });
 
 
