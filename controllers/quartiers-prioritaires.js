9605fa69b6d54dee364fc595b2e2e4dfa71c38c7
Nabil Servais <nabil@eml.cc>
2015-12-13 14:04:37 +0100

    Fix indent

--- controllers/quartiers-prioritaires.js
+++ controllers/quartiers-prioritaires.js
@@ -3,50 +3,50 @@ var _ = require('lodash');
 
 exports.layer = function(req, res, next) {
 
-  if (req.query.bbox) {
-    var bbox = req.query.bbox.split(',');
-  }
-  var sql = `SELECT ST_AsGeoJSON(qp.geom) as geom,
+    if (req.query.bbox) {
+        var bbox = req.query.bbox.split(',');
+    }
+    var sql = `SELECT ST_AsGeoJSON(qp.geom) as geom,
                       code_qp,
                       nom_qp,
                       commune_qp FROM quartiers_prioritaires as qp`;
-  if (bbox) {
-    sql += `,(select st_makeenvelope(${bbox.map(corner => corner)}, 4326) geom) b
+    if (bbox) {
+        sql += `,(select st_makeenvelope(${bbox.map(corner => corner)}, 4326) geom) b
                         where b.geom && qp.geom`;
-  }
-  req.pgClient.query(sql, function(err, result) {
-    if (err) {
-      req.pgEnd(err);
-      return next(err);
     }
+    req.pgClient.query(sql, function(err, result) {
+        if (err) {
+            req.pgEnd(err);
+            return next(err);
+        }
 
-    if (!result.rows) {
-      return res.status(404).send({
-        status: 'No Data'
-      });
-    }
+        if (!result.rows) {
+            return res.status(404).send({
+                status: 'No Data'
+            });
+        }
 
-    return res.send({
-      type: 'FeatureCollection',
-      features: result.rows.map(function(row) {
-        return {
-          type: 'Feature',
-          geometry: JSON.parse(row.geom),
-          properties: _.omit(row, 'geom')
-        };
-      })
+        return res.send({
+            type: 'FeatureCollection',
+            features: result.rows.map(function(row) {
+                return {
+                    type: 'Feature',
+                    geometry: JSON.parse(row.geom),
+                    properties: _.omit(row, 'geom')
+                };
+            })
+        });
     });
-  });
 };
 
 exports.search = function(req, res, next) {
-  if (!req.body.geo) {
-    res.status(400).send({
-      status: 'Missing param'
-    });
-  }
+    if (!req.body.geo) {
+        res.status(400).send({
+            status: 'Missing param'
+        });
+    }
 
-  req.pgClient.query(format(`
+    req.pgClient.query(format(`
         SELECT
             ST_AsGeoJSON(quartiers_prioritaires.geom) AS geom,
             code_qp AS code,
@@ -57,26 +57,26 @@ exports.search = function(req, res, next) {
             (SELECT ST_SetSRID(ST_GeomFromGeoJSON('%s'), 4326) geom) input
         WHERE ST_Intersects(quartiers_prioritaires.geom, input.geom);
     `, req.body.geo), function(err, result) {
-    if (err) {
-      req.pgEnd(err);
-      return next(err);
-    }
+        if (err) {
+            req.pgEnd(err);
+            return next(err);
+        }
 
-    if (!result.rows) {
-      return res.status(404).send({
-        status: 'No Data'
-      });
-    }
+        if (!result.rows) {
+            return res.status(404).send({
+                status: 'No Data'
+            });
+        }
 
-    return res.send({
-      type: 'FeatureCollection',
-      features: result.rows.map(function(row) {
-        return {
-          type: 'Feature',
-          geometry: JSON.parse(row.geom),
-          properties: _.omit(row, 'geom')
-        };
-      })
+        return res.send({
+            type: 'FeatureCollection',
+            features: result.rows.map(function(row) {
+                return {
+                    type: 'Feature',
+                    geometry: JSON.parse(row.geom),
+                    properties: _.omit(row, 'geom')
+                };
+            })
+        });
     });
-  });
 };
