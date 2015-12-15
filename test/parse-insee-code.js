4e81e683d591d03fbce29950d4f8437d16abe66a
Matti Schneider <matti.schneider@data.gouv.fr>
2015-12-15 16:28:11 +0100

    Refactor INSEE parsing tests

--- test/parse-insee-code.js
+++ test/parse-insee-code.js
@@ -3,45 +3,45 @@ const expect = require('expect.js');
 const parseInseeCode = require('../lib/parse-insee-code.js') ;
 
 describe('#parseCodeInsee()', () => {
-    it('will throw an exception for invalid INSEE code (length)', () => {
-        expect(parseInseeCode)
-            .withArgs('5444')
-            .to.throwException('INSEE code must have 5 characters');
+    describe('with invalid INSEE codes', () => {
+        [
+            '5444', // too short
+            '544477', // too long
+            '2E001',
+            '24A01',
+            '97801',
+            '00000'
+        ].forEach(invalidCode => {
+            it(`should reject ${invalidCode}`, () => {
+                expect(parseInseeCode)
+                    .withArgs(invalidCode)
+                    .to.throwException('Invalid INSEE code');
+            });
+        });
     });
 
-    it('will throw an exception for invalid INSEE code (content)', () => {
-        expect(parseInseeCode)
-            .withArgs('2E001')
-            .to.throwException('Invalid INSEE code');
-
-        expect(parseInseeCode)
-            .withArgs('24A01')
-            .to.throwException('Invalid INSEE code');
-
-        expect(parseInseeCode)
-            .withArgs('97801')
-            .to.throwException('Invalid DOM INSEE code');
-
-        expect(parseInseeCode)
-            .withArgs('00000')
-            .to.throwException('Invalid INSEE code');
-    });
-
-    it('should parse with success for basic INSEE code', () => {
+    it('should parse basic INSEE code', () => {
         expect(parseInseeCode('25349')).to.eql({
             code_dep: '25',
             code_com: '349'
         });
     });
 
-    it('should parse with success for DOM/TOM INSEE code', () => {
+    it('should parse an INSEE code in 93', () => {
+        expect(parseInseeCode('93233')).to.eql({
+            code_dep: '93',
+            code_com: '233'
+        });
+    });
+
+    it('should parse DOM/TOM INSEE code', () => {
         expect(parseInseeCode('97123')).to.eql({
             code_dep: '971',
             code_com: '23'
         });
     });
 
-    it('should parse with success for Corse INSEE code', () => {
+    it('should parse Corse INSEE code', () => {
         expect(parseInseeCode('2A001')).to.eql({
             code_dep: '2A',
             code_com: '001'
