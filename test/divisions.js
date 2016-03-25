46043883e0418b2dbde0f9949349ec30cea37d0f
vsagniez <vincent.sagniez@ign.fr>
2016-03-25 16:03:53 +0100

    Maj param dep et code arrondissement

--- test/divisions.js
+++ test/divisions.js
@@ -46,5 +46,25 @@ describe('test du module divisions', function() {
                 })
                 .end(done);
         });
+        it('should reply a FeatureCollection containing a valid Feature for case Paris 12e', done => {
+            request(server)
+                .get('/cadastre/division?insee=75056&codearr=112&section=AA')
+                .expect(res => {
+                    const feature = res.body.features[0];
+                    expect(feature.geometry.type).to.eql('MultiPolygon');
+                    expect(feature.properties).to.eql({
+                        feuille: 1,
+                        section: 'AA',
+                        code_dep: '75',
+                        nom_com: 'Paris',
+                        code_com: '056',
+                        com_abs: '000',
+                        echelle: '500',
+                        edition: 3,
+                        code_arr: '112'
+                    });
+                })
+                .end(done);
+        });
     });
 });
