e33fca445ea9172be418fc8878deb370ff640f1d
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-05-12 09:20:54 +0200

    MAJ référentiel Appellations viticoles (11/05/2017)

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
         password: process.env.PGPASSWORD || process.env.npm_package_config_pgPassword
     };
 
-    const appellationRelease = '20170410_Appellation';
+    const appellationRelease = '20170511_Appellation';
 
     const sourceFiles = {
         qp: refDataDir + '/qp-politiquedelaville-shp.zip',
