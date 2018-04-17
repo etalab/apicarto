4b5e484a7d42ac017d45a03c67f8cb6b9c160c17
Mickaël BORNE <mickael.borne@ign.fr>
2018-04-17 18:40:30 +0200

    aoc - tentative de chargement depuis data.gouv

---
+++ bin/import-appelation.js
@@ -0,0 +1,65 @@
+var shell = require('shelljs');
+var path = require('path');
+
+if (!shell.which('wget')) {
+	shell.echo('Sorry, this script requires wget');
+	shell.exit(1);
+}
+if (!shell.which('psql')) {
+	shell.echo('Sorry, this script requires psql');
+	shell.exit(1);
+}
+if (!shell.which('ogr2ogr')) {
+	shell.echo('Sorry, this script requires shp2pgsql');
+	shell.exit(1);
+}
+if (!shell.which('unzip')) {
+	shell.echo('Sorry, this script requires unzip');
+	shell.exit(1);
+}
+
+/* Download AOC file */
+var url = "https://www.data.gouv.fr/s/resources/delimitation-parcellaire-des-aoc-viticoles-de-linao/20180319-085833/delimitation_inao_EPSG2154.zip";
+
+var dataDir = __dirname+'/../data/aoc';
+if (shell.exec('mkdir -p '+dataDir).code !== 0) {
+	shell.echo('Fail to create data directory : '+dataDir)
+	shell.exit(1);
+}
+
+/* Change directory to data directory */
+shell.cd(dataDir);
+
+/* Download zip file if not exists */
+if ( ! shell.test('-e', 'delimitation_inao_EPSG2154.zip') ){ 
+	if (shell.exec('wget --progress=bar:force -O delimitation_inao_EPSG2154.zip '+url).code !== 0) {
+		shell.echo('Error: wget failed');
+		shell.exit(1);
+	}
+}
+
+/* Extract zip file */
+if (shell.exec('unzip -j -o delimitation_inao_EPSG2154.zip').code !== 0) {
+	shell.echo('Error: unzip failed');
+	shell.exit(1);
+}
+
+
+/* Convert shapefile to sql */
+var command = 'ogr2ogr --config PG_USE_COPY YES -f PGDump /vsistdout/ ';
+command += '-a_srs EPSG:2154 -t_srs EPSG:4326 -lco GEOMETRY_NAME=geom -lco DROP_TABLE=ON ';
+command += '-nlt PROMOTE_TO_MULTI -nln appellation ';
+command += 'delimitation_parcellaire_aoc_viticoles_inao.shp | psql --quiet';
+if (shell.exec(command).code !== 0) {
+	shell.echo('Error: import failed');
+	shell.exit(1);
+}
+
+
+
+/* post-traitement */
+var sqlPath = __dirname+'/../sql/prepare-appellations.sql';
+if (shell.exec('psql -f '+sqlPath).code !== 0) {
+	shell.echo('Error: psql failed');
+	shell.exit(1);
+}
--- sql/prepare-appellations.sql
+++ sql/prepare-appellations.sql
@@ -1,23 +1,24 @@
 -- Renommage de la colonne dont le nom est tronqué
-ALTER TABLE Appellation RENAME appellatio TO appellation;
+ALTER TABLE appellation RENAME appellatio TO appellation;
+ALTER TABLE appellation RENAME id_app TO idapp;
 
 -- Enrichissement du schéma de la table
 CREATE TYPE type_granularite_appellation AS ENUM('commune', 'exacte');
 
-ALTER TABLE Appellation ADD COLUMN instruction_obligatoire boolean;
-ALTER TABLE Appellation ADD COLUMN granularite type_granularite_appellation;
+ALTER TABLE appellation ADD COLUMN instruction_obligatoire boolean;
+ALTER TABLE appellation ADD COLUMN granularite type_granularite_appellation;
 
 -- Correction des géométries
-UPDATE Appellation SET geom = ST_MakeValid(ST_Buffer(geom, 0)) WHERE geom IS NOT NULL AND NOT ST_IsValid(geom);
+UPDATE appellation SET geom = ST_Multi(ST_MakeValid(ST_Buffer(geom, 0))) WHERE geom IS NOT NULL AND NOT ST_IsValid(geom);
 
 -- Enrichissement des données
-UPDATE Appellation SET instruction_obligatoire = FALSE;
-UPDATE Appellation SET granularite = 'exacte' WHERE segment = '1';
-UPDATE Appellation SET granularite = 'commune' WHERE segment IN ('3', '4');
+UPDATE appellation SET instruction_obligatoire = FALSE;
+UPDATE appellation SET granularite = 'exacte' WHERE segment = '1';
+UPDATE appellation SET granularite = 'commune' WHERE segment IN ('3', '4');
 
 -- Marquage de l'appellation Sable de Camargue
-UPDATE Appellation SET instruction_obligatoire = TRUE WHERE IDApp = '1022';
+UPDATE appellation SET instruction_obligatoire = TRUE WHERE IDApp = '1022';
 
 -- Création des indexes
-CREATE INDEX appellations_geom_gist ON Appellation USING GIST (geom);
-CREATE INDEX appellations_insee ON Appellation USING btree (insee);
+CREATE INDEX appellations_geom_gist ON appellation USING GIST (geom);
+CREATE INDEX appellations_insee ON appellation USING btree (insee);
