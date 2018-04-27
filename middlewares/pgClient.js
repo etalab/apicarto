109bdc193f7bd57a5c19cf39d64073fe1639cc28
MBorne <mborne@users.noreply.github.com>
2018-04-27 14:08:17 +0200

    postgresql : meilleure gestion des erreurs, doc : mise en forme

--- middlewares/pgClient.js
+++ middlewares/pgClient.js
@@ -13,13 +13,19 @@ module.exports = function(req, res, next) {
         password: 'postgis',
         port: 5432
     }*/);
-    req.pgClient.connect();
-
-    var _end = res.end; 
-    res.end = function(){
-        debug("close connection...");
-        req.pgClient.end();
-        _end.apply(this, arguments); 
-    };
-    next();
+    req.pgClient.connect().then(function(){
+        var _end = res.end; 
+        res.end = function(){
+            debug("close connection...");
+            req.pgClient.end();
+            _end.apply(this, arguments); 
+        };        
+        next();
+    }).catch(function(err){
+        console.log(err);
+        res.status(500).json({
+            'code': 500,
+            'message': 'Erreur de la connexion à la base de données'
+        });
+    });
 };
