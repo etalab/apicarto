f920e77d94310c8caea2a522fad434fa3a18c53f
vsagniez <vincent.sagniez@ign.fr>
2019-01-21 09:18:52 +0100

    Test Gestion cors IE11

--- middlewares/gpuWfsClient.js
+++ middlewares/gpuWfsClient.js
@@ -5,10 +5,11 @@ const GeoportalWfsClient = require('geoportal-wfs-client');
  * middleware pour la création du client geoportail
  */
 module.exports = function(req, res, next) {
-    var referer = 'http://localhost/';
+    var referer = 'apicarto.ign.fr';
 
     /* forward du referer du client */
     if ( req.headers.referer ){
+		console.log('je suis dans if');
         referer = req.headers.referer ;
     }
 
@@ -17,6 +18,11 @@ module.exports = function(req, res, next) {
         'url' : 'http://wxs-gpu.mongeoportail.ign.fr/externe/39wtxmgtn23okfbbs1al2lz3/wfs/v',
         'headers':{
             Referer: referer,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+            //'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept",
+            //'Access-Control-Allow-Methods': 'GET,POST',
+            'P3P' : CP="ALL IND DSP COR ADM CONo CUR CUSo IVAo IVDo PSA PSD TAI TELo OUR SAMo CNT COM INT NAV ONL PHY PRE PUR UNI",
             'User-Agent': 'apicarto'
         }
     });
