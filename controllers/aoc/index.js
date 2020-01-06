983941bcd657169aa854774af62d92b502ec59a6
vsagniez <vincent.sagniez@ign.fr>
2020-01-06 10:41:08 +0100

    Modification doc et resultats pour aoc

--- controllers/aoc/index.js
+++ controllers/aoc/index.js
@@ -39,31 +39,48 @@ router.post('/appellation-viticole', [
         LIMIT 1000
     `, params.geom );
     
-    req.pgClient.query(sql,function(err,result){
+    var sqlCommunes = format(`
+		SELECT
+        NOM_COM as nom,
+        CODE_INSEE as insee,
+        ST_Contains(input.geom, communes_ign.geom) AS contains,
+        ST_AsGeoJSON(communes_ign.geom) AS geom
+        FROM
+			communes_ign,
+            (SELECT ST_SetSRID(ST_GeomFromGeoJSON('%s'), 4326) geom) input
+        WHERE ST_Intersects(communes_ign.geom, input.geom);
+        `, params.geom);
+    
+    req.pgClient.query(sqlCommunes,function(err,result){  
         if (err)
+        return next(err);
+        req.intersectedCommunes = result.rows;    
+		req.pgClient.query(sql,function(err,result){
+			if (err)
             return next(err);
 
-        return res.send({
-            type: 'FeatureCollection',
-            features: result.rows.map(function (row) {
-                const feature = {
-                    type: 'Feature',
-                    geometry: JSON.parse(row.geom),
-                    properties: _.omit(row, 'geom')
-                };
-                /*
-                if (row.granularite === 'commune' && !row.instruction_obligatoire) {
-                    const commune = _.find(req.intersectedCommunes, { insee: row.insee });
-                    feature.properties.area = commune.intersect_area;
-                    feature.properties.contains = commune.contains;
-                    feature.geometry = JSON.parse(commune.geom);
-                }
-                */
-                return feature;
-            })
-        });
-    });
+			return res.send({
+				type: 'FeatureCollection',
+				features: result.rows.map(function (row) {
+					const feature = {
+						type: 'Feature',
+						geometry: JSON.parse(row.geom),
+					properties: _.omit(row, 'geom')
+					};
+					const communetest = _.find(req.intersectedCommunes, { insee: row.insee });
+					
+					if (row.granularite === 'commune' && !row.instruction_obligatoire) {
+						const commune = _.find(req.intersectedCommunes, { insee: row.insee });
+						feature.properties.area = commune.intersect_area;
+						feature.properties.contains = commune.contains;
+						feature.geometry = JSON.parse(commune.geom);
+					}
+                
+					return feature;
+				})
+			});
+		});
+	});
 });
-
 
 module.exports = router;
