c894e74a18c4a6c7df1ddcfa43b6b301e45cc7bc
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-03-10 11:08:39 +0100

    MAJ référentiel Appellations viticoles (02/03/2020)

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
         host: process.env.PGHOST || process.env.npm_package_config_pgHost || 'localhost'
     };
 
-    const appellationRelease = '20190701_Appellation_corr';
+    const appellationRelease = '20200302_Appellation';
 
     const sourceFiles = {
         qp: refDataDir + '/qp-politiquedelaville-shp.zip',
