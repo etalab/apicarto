2e279566d4c715b51c3e7f87cf68dd18c0e18dab
Vincent Sagniez <vincent.sagniez@ign.fr>
2022-04-14 11:46:39 +0200

    Suppression Apikey pour wfs-geoportail

---
+++ middlewares/geoportailWfsClient.js
@@ -0,0 +1,83 @@
+const debug = require('debug')('apicarto');
+const GeoportalWfsClient = require('geoportal-wfs-client');
+const readline = require("readline");
+const fs = require("fs");
+const { exit } = require('process');
+const { indexOf } = require('lodash');
+var fileSearchKey = "./datasets/wfs-geoportail/data/ressources_cle_wfs.csv";
+
+console.log
+
+function readFileKeys(path){
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: fs.createReadStream(path),
+            output: process.stdout,
+            terminal: false
+        });
+
+        let lines = [];
+        rl.on('line', (line) => {
+            lines.push(line);
+        });
+
+        rl.on('close', () => {
+            resolve(lines);
+        });
+    });
+};
+
+var linesKey =[];
+readFileKeys(fileSearchKey).then(function(result){
+    linesKey = result;
+});
+
+/*
+ * Middleware pour la création du client WFS geoportail
+ * 
+ * TODO permettre la définition de la clé au niveau du serveur
+ */
+module.exports = function(req, res, next) {
+    /* gestion des variables d'environnement et valeur par défaut */
+    var options = {
+        apiKey:  process.env.GEOPORTAL_API_KEY,
+        url: 'https://wxs.ign.fr/{apiKey}/geoportail/wfs',
+        headers:{
+            'User-Agent': 'apicarto',
+            'Referer': 'http://localhost'
+        }
+    };
+   
+    var recupKey = '';
+    let searchKeyIndice = -1;
+    let i=0;
+    // Recherche de la clé
+   while (searchKeyIndice < 0) {
+       lineSplit = linesKey[i].split(";");
+       if (lineSplit[3] == req.query.source) {
+           searchKeyIndice = 1;
+           recupKey = lineSplit[1];
+       } else {
+           i = i + 1;
+       }
+
+   }
+    /* gestion paramètre apikey */
+    var hasUserKey = false;
+    if ( recupKey ){
+        options.apiKey = recupKey;
+        hasUserKey = true;
+    }
+
+    /* gestion du paramètre Referer */
+    if ( req.headers.referer ){
+        options.headers.Referer = req.headers.referer ;
+    }
+    if ( process.env.GEOPORTAL_REFERER && ! hasUserKey ){
+        options.headers.Referer = process.env.GEOPORTAL_REFERER ;
+    }
+    
+    req.gppWfsClient = new GeoportalWfsClient(options);
+
+    next();
+};
