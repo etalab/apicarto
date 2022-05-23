045203923a1acf2bf61f3e3fd19848d7222c35f3
Vincent Sagniez <vincent.sagniez@ign.fr>
2022-05-23 16:06:32 +0200

    Maj sources correspondances

--- middlewares/geoportailWfsClient.js
+++ middlewares/geoportailWfsClient.js
@@ -2,7 +2,7 @@ const debug = require('debug')('apicarto');
 const GeoportalWfsClient = require('geoportal-wfs-client');
 const readline = require('readline');
 const fs = require('fs');
-var fileSearchKey = './datasets/wfs-geoportail/data/ressources_cle_wfs.csv';
+var fileSearchKey = './doc/data/ressources_cle_wfs2022-05-20.csv';
 
 function readFileKeys(path){
     return new Promise((resolve, reject) => {
