14ca5833eca6c425f477829fee326f45648c0259
MFrangi <manuel.frangi@ign.fr>
2023-07-12 12:53:36 +0000

    ajout nouveaux fichiers datasets

---
+++ datasets/appellations-viticoles/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    version: '8 mars 2023',
+    modules: ["Appellations viticoles"],
+    nom_url :"FranceAgriMer",
+    url: 'https://www.franceagrimer.fr/filieres-vin-et-cidre/vin/professionnels/teleprocedures'
+};
